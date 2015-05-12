de36889752b66937f885677c04cc27852d8e2502
shasto4ka <shasto4ka@mail.ru>
2015-05-12 22:28:52 +0300

    end_of_day

--- app/src/main/java/com/example/shasta/todolist/bye.java
+++ app/src/main/java/com/example/shasta/todolist/bye.java
@@ -58,6 +58,10 @@ public class bye extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_bye, container, false);
+
+
+
+
             return rootView;
         }
     }
