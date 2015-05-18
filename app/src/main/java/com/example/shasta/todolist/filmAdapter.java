2273322dc50f027f97e8a34a2bb0e1d8cc4cccb0
shasto4ka <shasto4ka@mail.ru>
2015-05-18 14:10:30 +0300

    bye_and_film_working

--- app/src/main/java/com/example/shasta/todolist/filmAdapter.java
+++ app/src/main/java/com/example/shasta/todolist/filmAdapter.java
@@ -2,7 +2,6 @@ package com.example.shasta.todolist;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
